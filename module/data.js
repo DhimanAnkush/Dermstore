638fc85696e495fffdf9b625b711ef21e3308e15
Ankush Dhiman <dhimanparteek444@gmail.com>
2022-04-10 00:09:11 +0530

    final

---
+++ module/data.js
@@ -0,0 +1,200 @@
+export var bio_products = [
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11291062-5634871742412089.jpg",
+        disp : "LUMIÈRE® Illuminating Eye Cream (0.5 fl. oz.)",
+        price : "$97.00",
+        sale: "Sale: $77.60",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11291541-5024871574576726.jpg",
+        disp : "Obagi Medical Nu-Derm Toner (6.7 fl. oz.)",
+        price : "$43.00",
+        sale: "Sale: $36.00",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/small/webp//productimg/original/12664555-1704871739992403.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        sale: "Sale: $94.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/small/webp//productimg/original/11289609-8744892770898772.jpg",
+        disp : " SkinCeuticals C E Ferulic (1 fl. oz.)",
+        price : "$166.00",
+        sale: "Sale: $156.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11291956-2074871578544985.jpg",
+        disp : "iS Clinical Active Serum (1 fl. oz.)",
+        price : "$142.00",
+        sale: "Sale: $120.00",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11174178-1044909106371632.jpg",
+        disp : "Paula's Choice SKIN PERFECTING 2 BHA Liquid Exfoliant (4 fl. oz.)",
+        price : "$32.00",
+        sale: "Sale: $27.00",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/small/webp//productimg/original/12664555-1704871739992403.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        sale: "Sale: $94.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11588281-8764882139037003.jpg",
+        disp : "Sunday Riley GOOD GENES All-In-One Lactic Acid Treatment (1.7 oz. - $175 Value)",
+        price : "$122.00",
+        sale: "Sale: $86.40",
+        discount : "17% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11290635-1804892767902966.jpg",
+        disp : "SkinCeuticals Triple Lipid Restore 242 (1.6 fl. oz.)",
+        price : "$130.00",
+        sale: "Sale: $94.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370303-1074944341868735.jpg",
+        disp : " EltaMD UV Clear Broad-Spectrum SPF 46 (1.7 oz.)",
+        price : "$37.00",
+        sale: "Sale: $29.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    }
+]
+
+export var brand_products = [
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12597163-5554871574934407.jpg",
+        head : "Olaplex",
+        disp : " No. 5 Bond Maintenance™ Conditioner (8.5 fl. oz.)",
+        price : "$28.0",
+        discount : "Earn 2x Dermstore Rewards Points",                       
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12901956-1634871508329232.jpg",
+        head : "Supergoop",
+        disp : "Unseen Sunscreen SPF 40 (1.7 fl. oz.)",
+        price : "$275.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902722-7844915030891827.jpg",
+        head : "Revision Skincare",
+        disp : " Brightening Facial Wash (6.7 fl. oz.)",
+        price : "$36.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13175699-2114876429714143.jpg",
+        head : "ILIA",
+        disp : " Limitless Lash Mascara - After Midnight (0.27 oz.)",
+        price : "$28.00",
+        discount : "Earn 2x Dermstore Rewards Points",                             
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370303-1074944341868735.jpg",
+        head : "EltaMD",
+        disp : "UV Clear Broad-Spectrum SPF 46 (1.7 oz.)",
+        price : "$36.0",
+        discount : "Earn 2x Dermstore Rewards Points",                       
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12453448-1934871739712818.jpg",
+        head : "Skin Care",
+        disp : " BIO SERUM FIRM Rejuvenating Growth Factor and Peptide Treatment (1 fl. oz.)",
+        price : "$65.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12902784-7634871308243767.jpg",
+        head : "Supergoop",
+        disp : "Glowscreen SPF 40 (1.7 fl. oz.)",
+        price : "$36.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13339906-1564915031255922.jpg",
+        head :"Revision Skincare",
+        disp : "  D.E.J eye cream® (0.5 oz.)",
+        price : "$100.00",
+        discount : "Earn 2x Dermstore Rewards Points",                             
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11370388-7024934560810401.jpg",
+        head : "Eminence Organic",
+        disp : "Strawberry Rhubarb Hyaluronic Serum (1 fl. oz.)",
+        price : "$48.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12915557-2954917112863726.jpg",
+        head : "Oribe",
+        disp : "Gold Lust Repair & Restore Shampoo (8.5 fl. oz.)",
+        price : "$49.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://static.thcdn.com/images/small/webp//productimg/original/12664555-1704871739992403.jpg",
+        head :"LUMIÈRE",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://static.thcdn.com/images/small/webp//productimg/original/12664554-1004871739941589.jpg",
+        head : "LUMIÈRE",
+        disp : " LUMIÈRE® FIRM Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$108.00",
+        discount : "Earn 2x Dermstore Rewards Points",                               
+    }
+]
+
+export var blog_product = [
+    {
+        img : "https://blogscdn.thehut.net/app/uploads/sites/1160/2019/10/Stocksy_txp7df3099dwFw200_Medium_3207140_1200x672-1_1629913680-700x392.jpg",
+        name : "How to Transition Your Skin Care Routine from Winter to Spring",
+        date : 'Jenn Sinrich 3/3/21'
+    },
+    {
+        img : "https://blogscdn.thehut.net/app/uploads/sites/1160/2018/08/080819-Feature-Order.jpg",
+        name : "In What Order Do I Apply My Skin Care Products?",
+        date : '3/2/21'
+    },
+    {
+        img : "https://blogscdn.thehut.net/app/uploads/sites/1160/2021/10/skinceuticals-products-feature_1634252473.jpg",
+        name : "7 Treatments That Work Double Duty While You Sleep",
+        date : 'Wendy Rodewald-Sulz 2/22/21'
+    },
+    {
+        img : "https://blogscdn.thehut.net/app/uploads/sites/1160/2018/10/100118-Body-Treatment.jpg",
+        name : "The Best Facial Treatments to Get During Fall and Winter, According to an Esthetician",
+        date : 'Janeca Racho  3/3/21'
+    },
+    {
+        img : "https://blogscdn.thehut.net/app/uploads/sites/1160/2021/12/winter-make-up_1639495440.jpg",
+        name : "The 9 Best Winter Makeup Products for Glowing Skin",
+        date : 'Janeca Racho  2/18/21'
+    }
+]
+
+
+
